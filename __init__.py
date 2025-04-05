463ef69adfeafa3d6ad76896374c84634c8528cf
Jo Kim <1893942+Jokimbe@users.noreply.github.com>
2025-04-05 14:56:42 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,5 @@
 """Top-level package for ComfyUI-DrawThings-gRPC."""
 
-__all__ = [
-    "NODE_CLASS_MAPPINGS",
-    "NODE_DISPLAY_NAME_MAPPINGS",
-    "WEB_DIRECTORY",
-]
-
 __author__ = """Jokimbe"""
 __version__ = "0.0.1"
 
@@ -13,3 +7,9 @@ from .nodes import NODE_CLASS_MAPPINGS
 from .nodes import NODE_DISPLAY_NAME_MAPPINGS
 
 WEB_DIRECTORY = "./web"
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    "WEB_DIRECTORY",
+]
