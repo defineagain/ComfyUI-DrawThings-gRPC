7b5a73566aa493e898f750c044f10cafc9a46963
Jo Kim <1893942+Jokimbe@users.noreply.github.com>
2025-04-09 15:59:27 +0200

    Update ComfyUI-DrawThings-gRPC.js

--- web/js/ComfyUI-DrawThings-gRPC.js
+++ web/js/ComfyUI-DrawThings-gRPC.js
@@ -1,5 +1,12 @@
-import { app } from "../../scripts/app.js";
-// import { app } from "http://localhost:8188/scripts/app.js";
+// import { app } from "../../scripts/app.js";
+import { app } from "http://localhost:8188/scripts/app.js";
+
+function hideWidgetDT(widgetObjects, hide) {
+    widgetObjects.forEach(listObjects);
+    function listObjects(widgetObject) {
+        widgetObject.disabled = hide;
+    }
+}
 
 app.registerExtension({
     name: "ComfyUI-DrawThings-gRPC",
@@ -10,46 +17,54 @@ app.registerExtension({
             node.widgets.forEach(listWidgets);
             function listWidgets(widget) {
                 widgetsList[widget.name] = widget;
-                console.log(widget.value);
-            }
-
-            function hideWidget(widgetNames, hide) {
-                widgetNames.forEach(listNames);
-                function listNames(widgetName) {
-                    widgetsList[widgetName].disabled = hide;
-                    console.log(widgetsList[widgetName]);
-                }
-            }
-
-            if (widgetsList["refiner"].value == true) {
-                hideWidget(["refiner_model", "refiner_start"], false);
-            } else {
-                hideWidget(["refiner_model", "refiner_start"], true);
+                // console.log(widget.value);
             }
 
             widgetsList["refiner"].callback = function () {
-                console.log(this.value);
+                // console.log(this.value);
                 if (this.value == true) {
-                    hideWidget(["refiner_model", "refiner_start"], false);
+                    hideWidgetDT([widgetsList["refiner_model"], widgetsList["refiner_start"]], false);
                 } else {
-                    hideWidget(["refiner_model", "refiner_start"], true);
+                    hideWidgetDT([widgetsList["refiner_model"], widgetsList["refiner_start"]], true);
                 }
             }
 
-            if (widgetsList["high_res_fix"].value == true) {
-                hideWidget(["high_res_fix_start_width", "high_res_fix_start_height", "high_res_fix_strength"], false);
-            } else {
-                hideWidget(["high_res_fix_start_width", "high_res_fix_start_height", "high_res_fix_strength"], true);
-            }
-
             widgetsList["high_res_fix"].callback = function () {
-                console.log(this.value);
+                // console.log(this.value);
                 if (this.value == true) {
-                    hideWidget(["high_res_fix_start_width", "high_res_fix_start_height", "high_res_fix_strength"], false);
+                    hideWidgetDT([widgetsList["high_res_fix_start_width"], widgetsList["high_res_fix_start_height"], widgetsList["high_res_fix_strength"]], false);
                 } else {
-                    hideWidget(["high_res_fix_start_width", "high_res_fix_start_height", "high_res_fix_strength"], true);
+                    hideWidgetDT([widgetsList["high_res_fix_start_width"], widgetsList["high_res_fix_start_height"], widgetsList["high_res_fix_strength"]], true);
                 }
             }
         }
     },
+    async afterConfigureGraph() {
+        // console.log(app.graph._nodes);
+        app.graph._nodes.forEach(listNodes);
+        function listNodes(node) {
+            if (node.type === "DrawThingsSampler") {
+                console.log(node);
+                const widgetsList = {};
+    
+                node.widgets.forEach(listWidgets);
+                function listWidgets(widget) {
+                    widgetsList[widget.name] = widget;
+                    // console.log(widget.value);
+                }
+
+                if (widgetsList["refiner"].value == true) {
+                    hideWidgetDT([widgetsList["refiner_model"], widgetsList["refiner_start"]], false);
+                } else {
+                    hideWidgetDT([widgetsList["refiner_model"], widgetsList["refiner_start"]], true);
+                }
+
+                if (widgetsList["high_res_fix"].value == true) {
+                    hideWidgetDT([widgetsList["high_res_fix_start_width"], widgetsList["high_res_fix_start_height"], widgetsList["high_res_fix_strength"]], false);
+                } else {
+                    hideWidgetDT([widgetsList["high_res_fix_start_width"], widgetsList["high_res_fix_start_height"], widgetsList["high_res_fix_strength"]], true);
+                }
+            }
+        }
+    }
 });
