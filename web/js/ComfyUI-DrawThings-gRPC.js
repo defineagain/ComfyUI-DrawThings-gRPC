a1fe50c487773b796c0e366fb69e466f3d9ecb82
Jo Kim <1893942+Jokimbe@users.noreply.github.com>
2025-04-09 12:39:06 +0200

    Update ComfyUI-DrawThings-gRPC.js

--- web/js/ComfyUI-DrawThings-gRPC.js
+++ web/js/ComfyUI-DrawThings-gRPC.js
@@ -4,33 +4,19 @@ import { app } from "../../scripts/app.js";
 
 app.registerExtension({
     name: "ComfyUI-DrawThings-gRPC",
-    async setup() {
-        console.log("Setup complete!");
-    },
     async nodeCreated(node) {
-        console.log("nodeCreated");
         if (node?.comfyClass === "DrawThingsSampler") {
-            const original_onMouseDown = node.onMouseDown;
-            node.onMouseDown = function (e, pos, canvas) {
-                console.log("Click!");
-                return original_onMouseDown?.apply(this, arguments);
-            };
-        }
-    },
-    async beforeRegisterNodeDef(nodeType, nodeData, app) {
-        if (nodeType.comfyClass == "DrawThingsSampler") {
-            const onConnectionsChange = nodeType.prototype.onConnectionsChange;
-            nodeType.prototype.onConnectionsChange = function (
-                side,
-                slot,
-                connect,
-                link_info,
-                output
-            ) {
-                const r = onConnectionsChange?.apply(this, arguments);
-                console.log("Someone changed my connection!");
-                return r;
-            };
+            node.widgets.forEach(listWidgets);
+            function listWidgets(widget) {
+                if (widget.name == "high_res_fix") {
+                    widget.callback = function () {
+                        // console.log(this);
+                        console.log(this.value);
+                        if (this.value) {
+                        }
+                    };
+                }
+            }
         }
     },
 });
