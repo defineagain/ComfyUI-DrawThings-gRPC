056babe0930e0613b65d658f167f7867df6ef090
Jo Kim <1893942+Jokimbe@users.noreply.github.com>
2025-04-10 23:55:48 +0200

    Previews for 1.5

--- nodes.py
+++ nodes.py
@@ -26,8 +26,7 @@ import struct
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "comfy"))
 
 import comfy.utils
-from comfy.cli_args import args, LatentPreviewMethod
-from comfy.taesd.taesd import TAESD
+from comfy.cli_args import args
 import latent_preview
 import comfy.latent_formats as latent_formats
 import comfy_execution.graph_utils as graph_utils
@@ -69,21 +68,13 @@ ModelsInfo = TypedDict('ModelsInfo', {
 MAX_RESOLUTION=16384
 MAX_PREVIEW_RESOLUTION = args.preview_size
 
-def prepare_callback(step, total_steps, x0=None, latent_format=None):
-    previewer = None
-    preview_format = "JPEG"
-    if preview_format not in ["JPEG", "PNG"]:
-        preview_format = "JPEG"
-
-    if latent_format is not None:
-        previewer = latent_preview.get_previewer(x0.device, latent_format)
-
+def prepare_callback(step, total_steps, x0=None):
     pbar = comfy.utils.ProgressBar(step)
     def callback(step, total_steps, x0=None):
 
         preview_bytes = None
-        if previewer is not None:
-            preview_bytes = previewer.decode_latent_to_preview_image(preview_format, x0)
+        if x0 is not None:
+            preview_bytes = ("PNG", x0, MAX_PREVIEW_RESOLUTION)
         pbar.update_absolute(step, total_steps, preview_bytes)
     return callback(step, total_steps, x0)
 
@@ -109,6 +100,97 @@ def convert_response_image(response_image: bytes):
         'height': height,
         'channels': channels,
     }
+
+def decode_preview(preview, version):
+    int_buffer = np.frombuffer(preview, dtype=np.uint32, count=17)
+    height, width, channels = int_buffer[6:9]
+
+    if channels not in {3, 4, 16}:
+        return None
+
+    offset = 68
+    length = width * height * channels * 2
+
+    # print(f"Received image is {width}x{height} with {channels} channels")
+    # print(f"Input size: {len(preview)} (Expected: {length + 68})")
+
+    fp16 = np.frombuffer(preview, dtype=np.float16, offset=offset)
+    # np_array = data.reshape(-1, channels, height, width)
+    # x0 = torch.from_numpy(np_array)
+    bytes_array = np.zeros((height, width, channels), dtype=np.uint8)
+
+    print(f'{version}')
+
+    image = None
+
+    match version:
+        case 'v1' | 'v2' | 'svdI2v':
+            for i in range(height * width):
+                v0, v1, v2, v3 = fp16[i * 4:i * 4 + 4]
+                r = 49.5210 * v0 + 29.0283 * v1 - 23.9673 * v2 - 39.4981 * v3 + 99.9368
+                g = 41.1373 * v0 + 42.4951 * v1 + 24.7349 * v2 - 50.8279 * v3 + 99.8421
+                b = 40.2919 * v0 + 18.9304 * v1 + 30.0236 * v2 - 81.9976 * v3 + 99.5384
+                
+                bytes_array[i // width, i % width] = [
+                    min(max(int(r if np.isfinite(r) else 0), 0), 255),
+                    min(max(int(g if np.isfinite(g) else 0), 0), 255),
+                    min(max(int(b if np.isfinite(b) else 0), 0), 255),
+                    255
+                ]
+            image = Image.fromarray(bytes_array, 'RGBA')
+
+        case 'sd3' | 'sd3Large':
+            # for i in range(height * width):
+            #     v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15 = \
+            #         fp16[i * 16:(i + 1) * 16]
+            #     r = (-0.0922 * v0 + 0.0311 * v1 + 0.1994 * v2 + 0.0856 * v3 + 0.0587 * v4 - 0.0006 * v5 + 0.0978 * v6 - 0.0042 * v7 - 0.0194 * v8 - 0.0488 * v9 + 0.0922 * v10 - 0.0278 * v11 + 0.0332 * v12 - 0.0069 * v13 - 0.0596 * v14 - 0.1448 * v15 + 0.2394) * 127.5 + 127.5
+            #     g = (-0.0175 * v0 + 0.0633 * v1 + 0.0927 * v2 + 0.0339 * v3 + 0.0272 * v4 + 0.1104 * v5 + 0.0306 * v6 + 0.1038 * v7 + 0.0020 * v8 + 0.0130 * v9 + 0.0988 * v10 + 0.0524 * v11 + 0.0456 * v12 - 0.0030 * v13 - 0.0465 * v14 - 0.1463 * v15 + 0.2135) * 127.5 + 127.5
+            #     b = (0.0749 * v0 + 0.0954 * v1 + 0.0458 * v2 + 0.0902 * v3 - 0.0496 * v4 + 0.0309 * v5 + 0.0427 * v6 + 0.1358 * v7 + 0.0669 * v8 - 0.0268 * v9 + 0.0951 * v10 - 0.0542 * v11 + 0.0895 * v12 - 0.0810 * v13 - 0.0293 * v14 - 0.1189 * v15 + 0.1925) * 127.5 + 127.5
+            #     bytes_array[i * 4] = max(min(int(r), 255), 0)
+            #     bytes_array[i * 4 + 1] = max(min(int(g), 255), 0)
+            #     bytes_array[i * 4 + 2] = max(min(int(b), 255), 0)
+            #     bytes_array[i * 4 + 3] = 255
+            # image = Image.frombytes('RGBA', (width, height), bytes(bytes_array))
+            image = None
+
+        case 'sdxlBase' | 'sdxlRefiner' | 'sdxl_base_v0.9' | 'ssd1b' | 'pixart' | 'auraflow':
+            # for i in range(height * width):
+            #     v0, v1, v2, v3 = fp16[i * 4:(i + 1) * 4]
+            #     r = 47.195 * v0 - 29.114 * v1 + 11.883 * v2 - 38.063 * v3 + 141.64
+            #     g = 53.237 * v0 - 1.4623 * v1 + 12.991 * v2 - 28.043 * v3 + 127.46
+            #     b = 58.182 * v0 + 4.3734 * v1 - 3.3735 * v2 - 26.722 * v3 + 114.5
+            #     bytes_array[i * 4] = max(min(int(r), 255), 0)
+            #     bytes_array[i * 4 + 1] = max(min(int(g), 255), 0)
+            #     bytes_array[i * 4 + 2] = max(min(int(b), 255), 0)
+            #     bytes_array[i * 4 + 3] = 255
+            # image = Image.frombytes('RGBA', (width, height), bytes_array)
+            image = None
+
+        case 'flux1':
+            # for i in range(height * width):
+            #     v = fp16[i * 16:i * 16 + 16]
+            #     r = (-0.0346 * v[0] + 0.0034 * v[1] + 0.0275 * v[2] - 0.0174 * v[3] +
+            #          0.0859 * v[4] + 0.0004 * v[5] + 0.0405 * v[6] - 0.0236 * v[7] -
+            #          0.0245 * v[8] + 0.1008 * v[9] - 0.0515 * v[10] + 0.0428 * v[11] +
+            #          0.0817 * v[12] - 0.1264 * v[13] - 0.0280 * v[14] - 0.1262 * v[15] - 0.0329) * 127.5 + 127.5
+            #     g = (0.0244 * v[0] + 0.0210 * v[1] - 0.0668 * v[2] + 0.0160 * v[3] +
+            #          0.0721 * v[4] + 0.0383 * v[5] + 0.0861 * v[6] - 0.0185 * v[7] +
+            #          0.0250 * v[8] + 0.0755 * v[9] + 0.0201 * v[10] - 0.0012 * v[11] +
+            #          0.0765 * v[12] - 0.0522 * v[13] - 0.0881 * v[14] - 0.0982 * v[15] - 0.0718) * 127.5 + 127.5
+            #     b = (0.0681 * v[0] + 0.0687 * v[1] - 0.0433 * v[2] + 0.0617 * v[3] +
+            #          0.0329 * v[4] + 0.0115 * v[5] + 0.0915 * v[6] - 0.0259 * v[7] +
+            #          0.1180 * v[8] - 0.0421 * v[9] + 0.0011 * v[10] - 0.0036 * v[11] +
+            #          0.0749 * v[12] - 0.1103 * v[13] - 0.0499 * v[14] - 0.0778 * v[15] - 0.0851) * 127.5 + 127.5
+                
+            #     bytes_array[i * 4] = min(max(int(r) if np.isfinite(r) else 0, 0), 255)
+            #     bytes_array[i * 4 + 1] = min(max(int(g) if np.isfinite(g) else 0, 0), 255)
+            #     bytes_array[i * 4 + 2] = min(max(int(b) if np.isfinite(b) else 0, 0), 255)
+            #     bytes_array[i * 4 + 3] = 255
+            # image = Image.fromarray(bytes_array.reshape((height, width, 4)), 'RGBA')
+            # print(f"size: {image.size}, mode: {image.mode}")
+            image = None
+
+    return image
 
 def convert_image_for_request(image_tensor: torch.Tensor, control_type=None):
 # Draw Things: C header + the Float16 blob of -1 to 1 values that represents the image (in RGB order and HWC format, meaning r(0, 0), g(0, 0), b(0, 0), r(1, 0), g(1, 0), b(1, 0) .... (r(x, y) represents the value of red at that particular coordinate). The actual header is a bit more complex, here is the reference: https://github.com/liuliu/s4nnc/blob/main/nnc/Tensor.swift#L1750 the ccv_nnc_tensor_param_t is here: https://github.com/liuliu/ccv/blob/unstable/lib/nnc/ccv_nnc_tfb.h#L79 The type is CCV_TENSOR_CPU_MEMORY, format is CCV_TENSOR_FORMAT_NHWC, datatype is CCV_16F (for Float16), dim is the dimension in N, H, W, C order (in the case it should be 1, actual height, actual width, 3).
@@ -402,43 +484,12 @@ async def dt_sampler(
             # print(f"current_step: {current_step}")
 
             if current_step:
-                img = None
                 if preview_image:
-# ComfyUI: An IMAGE is a torch.Tensor with shape [B,H,W,C], C=3. If you are going to save or load images, you will need to convert to and from PIL.Image format - see the code snippets below! Note that some pytorch operations offer (or expect) [B,C,H,W], known as ‘channel first’, for reasons of computational efficiency. Just be careful.
-# A LATENT is a dict; the latent sample is referenced by the key samples and has shape [B,C,H,W], with C=4.
                     x0 = None
-                    latent_format = None
                     modelinfo_version = DrawThingsLists.modelinfo_list["version"]
                     if modelinfo_version != "":
-                        print(f'version: {modelinfo_version}')
-
-                        if modelinfo_version in ("v1", "v2", "svdI2v"):
-                            latent_format = latent_formats.SD15(latent_formats.LatentFormat)
-                        if modelinfo_version[:3] == "sd3":
-                            latent_format = latent_formats.SD3()
-                        if modelinfo_version[:4] == "sdxl" or modelinfo_version in ("ssd1b", "pixart", "auraflow"):
-                            latent_format = latent_formats.SDXL()
-                        if modelinfo_version in ("flux1"):
-                            latent_format = latent_formats.Flux()
-                        if modelinfo_version in ("hunyuanVideo"):
-                            latent_format = latent_formats.HunyuanVideo()
-                        if modelinfo_version[:3] == "wan":
-                            latent_format = latent_formats.Wan21()
-                        # if modelinfo_version in ("kandinsky21"):
-                        # if modelinfo_version in ("wurstchenStageC", "wurstchenStageB"):
-
-                        result = convert_response_image(preview_image)
-                        if result is not None:
-                            data = result['data']
-                            width = result['width']
-                            height = result['height']
-                            channels = result['channels']
-
-                            np_array = data.reshape(-1, channels, height, width)
-                            x0 = torch.from_numpy(np_array).to(torch.float32)
-                            print(f"{x0.shape}")
-
-                prepare_callback(current_step, steps, x0, latent_format)
+                        x0 = decode_preview(preview_image, modelinfo_version)
+                prepare_callback(current_step, steps, x0)
 
             if generated_images:
                 images = []
