ae9ba44eee5aec7d5772839ae6c544df1cc826c0
Jo Kim <1893942+Jokimbe@users.noreply.github.com>
2025-04-15 16:46:31 +0200

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1014,7 +1014,7 @@ class DrawThingsControlNet:
                 "control_weight": ("FLOAT", {"default": 1.00, "min": 0.00, "max": 2.50, "step": 0.01, "tooltip": "How strongly to modify the diffusion model. This value can be negative."}),
                 "control_start": ("FLOAT", {"default": 0.00, "min": 0.00, "max": 1.00, "step": 0.01}),
                 "control_end": ("FLOAT", {"default": 1.00, "min": 0.00, "max": 1.00, "step": 0.01}),
-                "invert_image": ("BOOLEAN", {"default": False, "tooltip": "Some Control Nets (i.e. LineArt) need their image to be inverted."}),
+                "invert_image": ("BOOLEAN", {"default": False}),
             },
             "optional": {
                 "control_net": ("DT_CNET",),
