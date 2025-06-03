ab3c187f931ebcc3ac77345382dbb58978f55501
Jokimbe <1893942+Jokimbe@users.noreply.github.com>
2025-06-03 12:21:57 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """Top-level package for ComfyUI-DrawThings-gRPC."""
 
 __author__ = """Jokimbe"""
-__version__ = "0.0.4"
+__version__ = "0.0.13"
 
 from .nodes import NODE_CLASS_MAPPINGS
 from .nodes import NODE_DISPLAY_NAME_MAPPINGS
