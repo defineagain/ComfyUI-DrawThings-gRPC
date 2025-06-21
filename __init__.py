a0ad290980e22856e898d8ed27085ef34768bc3d
Kelly Jerrell <kcjerrell@gmail.com>
2025-06-20 21:19:57 -0700

    fix: no longer requires widget-inputs

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """Top-level package for ComfyUI-DrawThings-gRPC."""
 
 __author__ = """Jokimbe"""
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 
 from .src.nodes import NODE_CLASS_MAPPINGS
 from .src.nodes import NODE_DISPLAY_NAME_MAPPINGS
